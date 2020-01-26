8d43d43d44c64acee0af7a158d5b4ccb582182aa
jhbui1 <jhbui1@uci.edu>
2020-01-25 20:59:31 -0800

    api connection some ct form styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,21 +2,60 @@ import React, {Component} from 'react';
 import './styles/contact.scss';
 import Profile from './assets/profile.jpg';
 import {List,ListItem,ListItemContent} from 'react-mdl';
+import axios from 'axios';
 
 class Contact extends Component {
-  state = {show: false}
+  state = {
+    show: false,
+    name: '',
+    company: '',
+    email: '',
+    inquiry:'',
+  }
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({
+      ...this.state,
+      [event.target.name]:value
+    })
+  }
   showModal = (event) => {
     this.setState({show:true});
     event.stopPropagation();
   }
+
   hideModal = (event) => {
     this.setState({show:false});
     event.stopPropagation();
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({show:false});
+
+    const data = {
+      Name:this.state.name,
+      Email:this.state.email,
+      Company:this.state.company,
+      Inquiry:this.state.inquiry
+    };
+
+
+    console.log({data});
+    console.log({
+        "data": {"name": "Scott", "age": 25}
+    })
+
+    axios.post('https://sheetdb.io/api/v1/wqny5qzgwkl2d',{data})
+      .then(res=>{
+        console.log(res);
+      });
+    event.stopPropagation();
+  }
+
   render(){
     return(
       <div className="contact-content">
-
         <div className="contact-container">
           <div className="left">
             <h2> Jonathan Bui</h2>
@@ -48,9 +87,10 @@ class Contact extends Component {
             <button id="showContactForm" onClick={this.showModal}>
               Inquiry or Feedback?
             </button>
-            <Modal show={this.state.show} handleClose={this.hideModal} handleClick={this.showModal}>
+            <Modal show={this.state.show} handleClose={this.hideModal}
+              handleClick={this.showModal} handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}>
               <h3>Thank you for contacting me!</h3>
-
             </Modal>
           </div>
         </div>
@@ -59,7 +99,7 @@ class Contact extends Component {
   }
 }
 
-const Modal = ({ handleClose, handleClick, show, children }) => {
+const Modal = ({ handleClose, handleClick, show, handleSubmit, handleChange, children }) => {
   const showHideClassName = show ? 'modal display-block' : 'modal display-none';
   return (
     <div className={showHideClassName} onClick={handleClose}>
@@ -70,22 +110,29 @@ const Modal = ({ handleClose, handleClick, show, children }) => {
             <h4>Return information (optional)</h4>
             <div className="responder-fill-ins">
               <p>Name</p>
-              <input placeholder="Name"/>
+              <input type="text" name="name" onChange={handleChange}
+                placeholder="Name"/>
               <p>E-mail</p>
-              <input placeholder="E-mail"/>
+              <input type="text" name="email"  onChange={handleChange}
+                placeholder="E-mail"/>
               <p>Company</p>
-              <input placeholder="Company"/>
+              <input type="text" name="company" onChange={handleChange}
+                 placeholder="Company"/>
             </div>
           </div>
           <div className="inquiry-elements">
             <h4>Inquiry or Feedback:</h4>
-            <textarea id="contact-txt-area"></textarea>
+            <textarea id="contact-txt-area" onChange={handleChange} name="inquiry"></textarea>
           </div>
         </div>
-        <button  id="close-btn" onClick={handleClose}>
-          Close
-        </button>
-
+        <div className="form-btns">
+          <button id="close" class="btn" onClick={handleClose}>
+            Close
+          </button>
+          <button id="submit" class="btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
